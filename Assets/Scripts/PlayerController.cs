61db13d44406c9893299e8cfc772daef153ddd39
Jimmy Liu <liuzhenming@ownself.org>
2015-04-18 21:25:27 +0800

    Update Code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,13 @@ public class PlayerController : MonoBehaviour {
 	private Animator _anim;
 	private Rigidbody2D _rb2d;
 	private float _jumpInterval = 0;
+	private GameObject waterTrail;
 
 	// Use this for initialization
 	void Start() {
 		_anim = GetComponent<Animator>();
 		_rb2d = GetComponent<Rigidbody2D>();
+		waterTrail = GameObject.Find("WaterTrail");
 	}
 
 	void Update() {
@@ -36,19 +38,31 @@ public class PlayerController : MonoBehaviour {
 		{
 			_anim.SetFloat("leftInput", 1);
 			_rb2d.velocity = new Vector2(-3, _rb2d.velocity.y);
-			GameObject waterTrail = GameObject.Find("WaterTrail");
-			Quaternion rot = waterTrail.transform.localRotation;
-			waterTrail.transform.localRotation = new Quaternion(rot.x, -90, rot.z, rot.w);
+			Vector3 rot = waterTrail.transform.eulerAngles;
+			waterTrail.transform.eulerAngles = new Vector3(rot.x, -90, rot.z);
+			if (waterTrail.GetComponent<ParticleSystem>().isPlaying) {
+				waterTrail.GetComponent<ParticleSystem>().Stop();
+				// waterTrail.SetActive(false);
+			}
+			return;
 		}
 
 		if (Input.GetKey("right") || Input.GetKey("d"))
 		{
 			_anim.SetFloat("rightInput", 1);
 			_rb2d.velocity = new Vector2(3, _rb2d.velocity.y);
-			Quaternion rot = GameObject.Find("WaterTrail").transform.localRotation;
-			Debug.Log(rot);
-			GameObject.Find("WaterTrail").transform.localRotation = new Quaternion(rot.x, 180, rot.z, rot.w);
+			Vector3 rot = waterTrail.transform.eulerAngles;
+			waterTrail.transform.eulerAngles = new Vector3(rot.x, 90, rot.z);
+			if (waterTrail.GetComponent<ParticleSystem>().isPlaying) {
+				waterTrail.GetComponent<ParticleSystem>().Stop();
+				// waterTrail.SetActive(false);
+			}
+			return;
 		}
 
+		// if (!waterTrail.GetComponent<ParticleSystem>().isPlaying) {
+			// waterTrail.SetActive(true);
+			waterTrail.GetComponent<ParticleSystem>().Play();
+		// }
 	}
 }
