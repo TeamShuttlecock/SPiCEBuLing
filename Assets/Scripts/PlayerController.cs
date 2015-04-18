064d4451134076728bd78d91a35734fc456d9be2
Jimmy Liu <liuzhenming@ownself.org>
2015-04-18 18:45:18 +0800

    Update player controller
    Also upload pee sprite

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,26 +4,31 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	private Animator _anim;
+	private Rigidbody2D _rb2d;
 
 	// Use this for initialization
-	void Start () {
+	void Start() {
 		_anim = GetComponent<Animator>();
+		_rb2d = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate() {
 
 		_anim.SetFloat("leftInput", 0);
 		_anim.SetFloat("rightInput", 0);
+		_rb2d.velocity = new Vector2(0, _rb2d.velocity.y);
 
 		if (Input.GetKey("left") || Input.GetKey("a"))
 		{
 			_anim.SetFloat("leftInput", 1);
+			_rb2d.velocity = new Vector2(-3, _rb2d.velocity.y);
 		}
 
 		if (Input.GetKey("right") || Input.GetKey("d"))
 		{
 			_anim.SetFloat("rightInput", 1);
+			_rb2d.velocity = new Vector2(3, _rb2d.velocity.y);
 		}
 
 	}
