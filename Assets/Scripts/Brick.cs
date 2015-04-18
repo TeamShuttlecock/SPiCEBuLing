ca4b0acdb2f95fc9a35deb8b611f383177b66501
quziqin <ziqin.qu@gmail.com>
2015-04-18 19:49:17 +0800

    Adjust spawn behavior

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -3,9 +3,6 @@ using System.Collections;
 
 public class Brick : MonoBehaviour {
 
-    public float Width = 0.0f;
-    public float Height = 0.0f;
-
 	// Use this for initialization
 	void Start () {
 	
--- Assets/Scripts/PlatformSpawner.cs
+++ Assets/Scripts/PlatformSpawner.cs
@@ -7,11 +7,12 @@ public class PlatformSpawner : MonoBehaviour {
     public List<GameObject> BrickPrefabs = new List<GameObject>();
 
     public int FloorNumber = 0;
-    public int BrickNumberPerFloorMin = 0;
     public int BrickNumberPerFloorMax = 0;
 
     public float IntervalX = 0.0f;
     public float IntervalY = 0.0f;
+
+    public float BrickBaseWidth = 0.0f;
 
     protected List<List<Brick>> mBrickCollection = new List<List<Brick>>();
     protected List<Transform> mFloorRoots = new List<Transform>();
@@ -36,32 +37,38 @@ public class PlatformSpawner : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
+        Init();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    if(Input.GetKeyUp(KeyCode.X))
+	    if(Input.GetKey(KeyCode.X))
         {
+            ClearPlatforms();
             SpawnPlatforms();
         }
 	}
 
-    public void SpawnPlatforms()
+    public void Init()
     {
-        int brickNum = 0;
-
-        float currentPosX = 0.0f;
-
-        for (int y = 0; y < FloorNumber; y++)
+        for(int i=0; i<FloorNumber; i++)
         {
-            currentPosX = 0.0f;
-            brickNum = Random.Range(BrickNumberPerFloorMin, BrickNumberPerFloorMax);
             mBrickCollection.Add(new List<Brick>());
-
-            if(mFloorRoots.Count <= y)
+            for(int j=0; j<BrickNumberPerFloorMax; j++)
             {
-                GameObject floorGo = new GameObject("Floor_" + y);
+                mBrickCollection[i].Add(null);
+            }
+        }
+    }
+
+    public void ClearPlatforms()
+    {
+        for (int i = 0; i < mBrickCollection.Count; i++)
+        {
+            if (mFloorRoots.Count <= i)
+            {
+                GameObject floorGo = new GameObject("Floor_" + i);
                 Transform floorRoot = floorGo.GetComponent<Transform>();
                 if (floorRoot == null)
                     floorRoot = floorGo.AddComponent<Transform>();
@@ -71,20 +78,84 @@ public class PlatformSpawner : MonoBehaviour {
                 mFloorRoots.Add(floorRoot);
             }
 
-            for (int x = 0; x < brickNum; x++)
+            for (int j = 0; j < mBrickCollection[i].Count; j++ )
+            {
+                if (mBrickCollection[i][j] != null)
+                {
+                    CacheManager.Instance.DestroyCachedObject(mBrickCollection[i][j].gameObject);
+
+                    mBrickCollection[i][j] = null;
+                }
+            }
+        }
+    }
+
+    public void SpawnPlatforms()
+    {
+        float currentPosX = 0.0f;
+
+        for (int y = 0; y < FloorNumber; y++)
+        {
+            currentPosX = 0.0f;
+
+            int factor = BrickNumberPerFloorMax;
+
+            for (int x = 0; x < BrickNumberPerFloorMax; x++)
             {
                 int brickIdx = Random.Range(0, BrickPrefabs.Count);
-
-                GameObject brickGo = (GameObject)CacheManager.Instance.InstantiateObject(BrickPrefabs[brickIdx]);
-                Brick brick = brickGo.GetComponent<Brick>();
-
-                Vector3 pos = new Vector3(currentPosX + IntervalX + brick.Width / 2, y * IntervalY);
-
-                brickGo.transform.position = pos;
-                brickGo.transform.parent = mFloorRoots[y];
-
+                Vector3 pos = new Vector3(currentPosX + IntervalX + BrickBaseWidth / 2, (FloorNumber - y - 1) * IntervalY);
                 currentPosX = pos.x;
-                mBrickCollection[y].Add(brick);
+
+                bool shouldSpawn = false;
+
+                int upLeftX = x - 1;
+                int upY = y - 1;
+
+                int upRightX = x + 1;
+
+                if(upY >= 0 && upY < FloorNumber)
+                {
+                    if (mBrickCollection[upY][x] != null)
+                    {
+                        shouldSpawn = false;
+                    }
+                    else
+                    {
+                        if (upLeftX >= 0 && upLeftX < BrickNumberPerFloorMax)
+                        {
+                            if (mBrickCollection[upY][upLeftX] != null)
+                            {
+                                shouldSpawn = true;
+                            }
+                        }
+                        else if (upRightX >= 0 && upRightX < BrickNumberPerFloorMax)
+                        {
+                            if (mBrickCollection[upY][upRightX] != null)
+                            {
+                                shouldSpawn = true;
+                            }
+                        }
+                    }
+                }
+
+                if(!shouldSpawn)
+                {
+                    shouldSpawn = Random.Range(0, factor) == 0;
+                }
+
+                if(shouldSpawn)
+                {
+                    GameObject brickGo = (GameObject)CacheManager.Instance.InstantiateObject(BrickPrefabs[brickIdx]);
+                    Brick brick = brickGo.GetComponent<Brick>();
+                    brickGo.transform.position = pos;
+                    brickGo.transform.parent = mFloorRoots[y];
+
+                    mBrickCollection[y][x] = brick;
+                }
+                else
+                {
+                    factor--;
+                }
             }
         }
     }
