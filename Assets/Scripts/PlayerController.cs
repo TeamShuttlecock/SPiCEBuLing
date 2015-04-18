9a643eaed8b59a031f01d3068934ddadb1c87c8f
Jimmy Liu <liuzhenming@ownself.org>
2015-04-18 19:08:36 +0800

    Upload Scripts

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,20 @@ public class PlayerController : MonoBehaviour {
 
 	private Animator _anim;
 	private Rigidbody2D _rb2d;
+	private float _jumpInterval = 0;
 
 	// Use this for initialization
 	void Start() {
 		_anim = GetComponent<Animator>();
 		_rb2d = GetComponent<Rigidbody2D>();
 	}
-	
+
+	void Update() {
+		if (_jumpInterval > 0) {
+			_jumpInterval -= Time.deltaTime;
+		}
+	}
+
 	// Update is called once per frame
 	void FixedUpdate() {
 
@@ -31,5 +38,11 @@ public class PlayerController : MonoBehaviour {
 			_rb2d.velocity = new Vector2(3, _rb2d.velocity.y);
 		}
 
+		// if (_jumpInterval <= 0.0f) {
+			if (Input.GetKeyDown("space") || Input.GetKeyDown("w") || Input.GetKeyDown("up")) {
+				_rb2d.AddForce(Vector2.up * 300);
+				_jumpInterval = 2.0f;
+			}
+		// }
 	}
 }
