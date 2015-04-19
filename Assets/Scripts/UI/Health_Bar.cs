c1ae513bc6d6ca19200b27963ad90db2228a0a06
madjojoshuyan <mad_jojoshuyan@hotmail.com>
2015-04-19 17:10:44 +0800

    MainScreen & FxScene Update

---
+++ Assets/Scripts/UI/Health_Bar.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI ;
+public class Health_Bar : MonoBehaviour {
+	public float Get_Value ;
+	public Image Bar_Image;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Mathf.Clamp01 (Get_Value);
+		Bar_Image.fillAmount = Get_Value;
+	}
+}
