c25c132fa80e5d79f63bddae4c900395a9a41154
madjojoshuyan <mad_jojoshuyan@hotmail.com>
2015-04-20 22:03:43 +0800

    fire type update
    现在有一个叫FirePrefab的Prefab嘛，在Fire Type里输入1~3可以在视觉上改变火的样子，刘爷看看能不能用得上哈

---
+++ Assets/Scripts/FX/Fire_Visual_Control.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire_Visual_Control : MonoBehaviour {
+
+    public GameObject firePrefab, smokePrefab, extinctionPrefab;
+    public int fireType =1;
+
+    public void ChangeFireType(int curFireType)
+    { 
+        
+        if (curFireType > 0 || curFireType < 4)
+        {
+            fireType = curFireType;
+        }
+        else return;
+    }
+
+    private void allFireType()
+    {
+        fireType = Mathf.Clamp(fireType, 1, 3);
+        switch (fireType)
+        { 
+            case 1:
+                firePrefab.SetActive(true);
+                smokePrefab.SetActive(false);
+                extinctionPrefab.SetActive(false);
+                print("nomal");
+                break;
+            case 2:
+                firePrefab.SetActive(true);
+                smokePrefab.SetActive(true);
+                extinctionPrefab.SetActive(false);
+                print("smoking");
+                break;
+            case 3:
+                firePrefab.SetActive(false);
+                smokePrefab.SetActive(false);
+                extinctionPrefab.SetActive(true);
+                print("extinction");
+                break;
+        }
+    }
+    private void Init()
+    {
+        firePrefab.SetActive(false);
+        smokePrefab.SetActive(false);
+        extinctionPrefab.SetActive(false);
+    }
+	// Use this for initialization
+	void Start () {
+        Init();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        allFireType();
+	}
+}
